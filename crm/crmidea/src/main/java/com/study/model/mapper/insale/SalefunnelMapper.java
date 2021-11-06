82fb733a0dfb22817e3765c035a9a707a5267e7b
MyKings000 <2371400588@qq.com>
2021-11-06 15:05:51 +0800

    销售机会mapper文件修改

--- crm/crmidea/src/main/java/com/study/model/mapper/insale/SalefunnelMapper.java
+++ crm/crmidea/src/main/java/com/study/model/mapper/insale/SalefunnelMapper.java
@@ -1,11 +1,14 @@
 package com.study.model.mapper.insale;
 
+import com.study.model.pojo.insale.Salefunnel;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
-@Mapper
+import java.util.List;
+
 @Repository
+@Mapper
 public interface SalefunnelMapper {
 
-
+    public List<Salefunnel> selectAll();
 }
