0d05e75bc24a86446b80fcade09739e1517f48e8
llw <1580958661@qq.com>
2021-11-06 11:29:20 +0800

    llw1

---
+++ crm/crmidea/src/main/java/com/study/model/pojo/aftersale/Invoice.java
@@ -0,0 +1,187 @@
+package com.study.model.pojo.aftersale;
+
+
+public class Invoice {
+
+  private long invoiceId;
+  private long invoicePjid;
+  private String invoiceName;
+  private long invoiceDdid;
+  private long invoiceQcid;
+  private double invoicePrice;
+  private long invoiceJbr;
+  private String invoicePjtt;
+  private String invoiceNssbh;
+  private long invoicePjyjzt;
+  private String invoiceSjr;
+  private String invoicePhone;
+  private String invoiceBz;
+  private long invoiceZt;
+  private String invoiceKhyh;
+  private String invoiceKhzh;
+  private String invoiceKhdz;
+  private long invoiceKhid;
+
+
+  public long getInvoiceId() {
+    return invoiceId;
+  }
+
+  public void setInvoiceId(long invoiceId) {
+    this.invoiceId = invoiceId;
+  }
+
+
+  public long getInvoicePjid() {
+    return invoicePjid;
+  }
+
+  public void setInvoicePjid(long invoicePjid) {
+    this.invoicePjid = invoicePjid;
+  }
+
+
+  public String getInvoiceName() {
+    return invoiceName;
+  }
+
+  public void setInvoiceName(String invoiceName) {
+    this.invoiceName = invoiceName;
+  }
+
+
+  public long getInvoiceDdid() {
+    return invoiceDdid;
+  }
+
+  public void setInvoiceDdid(long invoiceDdid) {
+    this.invoiceDdid = invoiceDdid;
+  }
+
+
+  public long getInvoiceQcid() {
+    return invoiceQcid;
+  }
+
+  public void setInvoiceQcid(long invoiceQcid) {
+    this.invoiceQcid = invoiceQcid;
+  }
+
+
+  public double getInvoicePrice() {
+    return invoicePrice;
+  }
+
+  public void setInvoicePrice(double invoicePrice) {
+    this.invoicePrice = invoicePrice;
+  }
+
+
+  public long getInvoiceJbr() {
+    return invoiceJbr;
+  }
+
+  public void setInvoiceJbr(long invoiceJbr) {
+    this.invoiceJbr = invoiceJbr;
+  }
+
+
+  public String getInvoicePjtt() {
+    return invoicePjtt;
+  }
+
+  public void setInvoicePjtt(String invoicePjtt) {
+    this.invoicePjtt = invoicePjtt;
+  }
+
+
+  public String getInvoiceNssbh() {
+    return invoiceNssbh;
+  }
+
+  public void setInvoiceNssbh(String invoiceNssbh) {
+    this.invoiceNssbh = invoiceNssbh;
+  }
+
+
+  public long getInvoicePjyjzt() {
+    return invoicePjyjzt;
+  }
+
+  public void setInvoicePjyjzt(long invoicePjyjzt) {
+    this.invoicePjyjzt = invoicePjyjzt;
+  }
+
+
+  public String getInvoiceSjr() {
+    return invoiceSjr;
+  }
+
+  public void setInvoiceSjr(String invoiceSjr) {
+    this.invoiceSjr = invoiceSjr;
+  }
+
+
+  public String getInvoicePhone() {
+    return invoicePhone;
+  }
+
+  public void setInvoicePhone(String invoicePhone) {
+    this.invoicePhone = invoicePhone;
+  }
+
+
+  public String getInvoiceBz() {
+    return invoiceBz;
+  }
+
+  public void setInvoiceBz(String invoiceBz) {
+    this.invoiceBz = invoiceBz;
+  }
+
+
+  public long getInvoiceZt() {
+    return invoiceZt;
+  }
+
+  public void setInvoiceZt(long invoiceZt) {
+    this.invoiceZt = invoiceZt;
+  }
+
+
+  public String getInvoiceKhyh() {
+    return invoiceKhyh;
+  }
+
+  public void setInvoiceKhyh(String invoiceKhyh) {
+    this.invoiceKhyh = invoiceKhyh;
+  }
+
+
+  public String getInvoiceKhzh() {
+    return invoiceKhzh;
+  }
+
+  public void setInvoiceKhzh(String invoiceKhzh) {
+    this.invoiceKhzh = invoiceKhzh;
+  }
+
+
+  public String getInvoiceKhdz() {
+    return invoiceKhdz;
+  }
+
+  public void setInvoiceKhdz(String invoiceKhdz) {
+    this.invoiceKhdz = invoiceKhdz;
+  }
+
+
+  public long getInvoiceKhid() {
+    return invoiceKhid;
+  }
+
+  public void setInvoiceKhid(long invoiceKhid) {
+    this.invoiceKhid = invoiceKhid;
+  }
+
+}
